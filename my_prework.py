8e2ff9d11084064b20984a18effcbe90a26f8e17
Syed Hussain Ather <shussainather@gmail.com>
2023-05-22 12:57:03 -0400

    one liner for leap year

--- my_prework.py
+++ my_prework.py
@@ -49,17 +49,8 @@ def is_leap_year(a_year):
     """
     Is is a leap year? 
     """
-    if a_year % 4 == 0:
-        if a_year % 100 == 0:
-            if a_year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
-    
+    return a_year % 4 == 0 and (a_year % 100 != 0 or a_year % 400 == 0)
+ 
 """
 Question 5:
 Write a function to check to see if all numbers in list are consecutive numbers. 
